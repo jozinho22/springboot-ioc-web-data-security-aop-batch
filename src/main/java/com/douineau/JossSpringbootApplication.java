630dbe4d4eb52fbe3db23ef15243589e50fe74d9
jozinho22 <josselin.douineau.1987@gmail.com>
2020-12-19 19:32:04 +0100

    ajout de l'annot @EnableCongifProperties

--- src/main/java/com/douineau/JossSpringbootApplication.java
+++ src/main/java/com/douineau/JossSpringbootApplication.java
@@ -7,12 +7,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.douineau.annotation.JozinhoApi;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 //  @SpringBootApplication replaces this 3 annotations :
 //  - @Configuration
 //  - @EnableAutoConfiguration
 //  - @ComponentScan
 @SpringBootApplication
+@EnableConfigurationProperties(CustomProperties.class)
 public class JossSpringbootApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/douineau/util/CustomProperties.java
+++ src/main/java/com/douineau/util/CustomProperties.java
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 @ConfigurationProperties("jozinho")
-@Service
 public class CustomProperties {
 
     private String name = "NAME-TEST";
